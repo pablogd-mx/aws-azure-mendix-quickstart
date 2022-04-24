3e3c6da2b8e7b6cdd7adb6f3d9bc92ce61d8aecc
EC2 Default User <ec2-user@ip-172-31-36-34.eu-west-1.compute.internal>
2022-04-24 10:39:14 +0000

    Adding AZ cli installation, removing extra lines

--- install-everything_aws.sh
+++ install-everything_aws.sh
@@ -1,4 +1,5 @@
-echo "Install OS tools"
+echo "The following tools will be installed:"
+echo "aws cli, az cli, kubectl, eksctl, jq, helm, pip3"
 sudo yum -y -q -e 0 install  jq moreutils nmap > /dev/null
 echo "Update OS tools"
 sudo yum update -y > /dev/null
@@ -16,26 +17,7 @@ sudo ./aws/install > /dev/null
 rm -f awscliv2.zip
 rm -rf aws
 
-# setup for AWS cli
-  rm -vf ${HOME}/.aws/credentials
-  export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
-  export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
-  export AZS=($(aws ec2 describe-availability-zones --query 'AvailabilityZones[].ZoneName' --output text --region $AWS_REGION))
-  export TF_VAR_region=${AWS_REGION}
-  test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || echo "AWS_REGION is not set !!"
-  echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
-  echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
-  echo "export AZS=(${AZS[@]})" | tee -a ~/.bash_profile
-  echo "export TF_VAR_region=${AWS_REGION}" | tee -a ~/.bash_profile
-  
-  aws configure set default.region ${AWS_REGION}
-  aws configure get region
 
-# echo "Setup Terraform cache"
-# if [ ! -f $HOME/.terraform.d/plugin-cache ];then
-#   mkdir -p $HOME/.terraform.d/plugin-cache
-#   cp tf-setup/dot-terraform.rc $HOME/.terraformrc
-# fi
 echo "Setup kubectl"
 if [ ! `which kubectl 2> /dev/null` ]; then
   echo "Install kubectl"
@@ -71,106 +53,26 @@ if [ ! `which kubectx 2> /dev/null` ]; then
 fi
 
 
-
-
-#
-# echo "ssh key"
-# if [ ! -f ~/.ssh/id_rsa ]; then
-#   mkdir -p ~/.ssh
-#   ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
-#   chmod 600 ~/.ssh/id*
-# fi
-# #
-# echo "ssm cli add on"
-# curl --silent "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
-# sudo yum install -y session-manager-plugin.rpm > /dev/null
-# rm -f ~/environment/session-manager-plugin.rpm
-# #
-# echo "install tfsec ..."
-# wget -q https://github.com/aquasecurity/tfsec/releases/download/v0.56.0/tfsec-linux-amd64
-# sudo mv tfsec-linux-amd64 /usr/bin/tfsec
-# sudo chmod 755 /usr/bin/tfsec 
-#
-
-
-echo "pip3"
-curl --silent "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-python3 get-pip.py 2&> /dev/null
-# echo "git-remote-codecommit"
-# pip3 install git-remote-codecommit 2&> /dev/null
-
-# # ------  resize OS disk -----------
-# # Specify the desired volume size in GiB as a command-line argument. If not specified, default to 32 GiB.
-# VOLUME_SIZE=${1:-32}
-
-# # Get the ID of the environment host Amazon EC2 instance.
-# INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data//instance-id)
-
-# # Get the ID of the Amazon EBS volume associated with the instance.
-# VOLUME_ID=$(aws ec2 describe-instances \
-#   --instance-id $INSTANCE_ID \
-#   --query "Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId" \
-#   --output text)
-
-# # Resize the EBS volume.
-# aws ec2 modify-volume --volume-id $VOLUME_ID --size $VOLUME_SIZE > /dev/null
-
-# # Wait for the resize to finish.
-# while [ \
-#   "$(aws ec2 describe-volumes-modifications \
-#     --volume-id $VOLUME_ID \
-#     --filters Name=modification-state,Values="optimizing","completed" \
-#     --query "length(VolumesModifications)"\
-#     --output text)" != "1" ]; do
-# sleep 1
-# done
-
-# if [ $(readlink -f /dev/xvda) = "/dev/xvda" ]
-# then
-#   # Rewrite the partition table so that the partition takes up all the space that it can.
-#   sudo growpart /dev/xvda 1
- 
-#   # Expand the size of the file system.
-#   sudo resize2fs /dev/xvda1 > /dev/null
-
-# else
-#   # Rewrite the partition table so that the partition takes up all the space that it can.
-#   sudo growpart /dev/nvme0n1 1
-
-#   # Expand the size of the file system.
-#   # sudo resize2fs /dev/nvme0n1p1 #(Amazon Linux 1)
-#   sudo xfs_growfs /dev/nvme0n1p1 > /dev/null #(Amazon Linux 2)
-# fi
-# df -m /
-#
-#
+" Installing AZ CLI"
+curl -L https://aka.ms/InstallAzureCli | bash
 
 echo "Verify ...."
-for command in jq aws wget kubectl eksctl helm kubectx
+for command in jq aws wget kubectl eksctl helm kubectx az
   do
     which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
   done
 
 
-this=`pwd`
-#echo "sample apps"
-cd ~/environment
-
-echo "Enable bash_completion"
-. /etc/profile.d/bash_completion.sh
-. ~/.bash_completion
-echo "alias tfb='terraform init && terraform plan -out tfplan && terraform apply tfplan && terraform init -force-copy'" >> ~/.bash_profile
-echo "alias aws='/usr/local/bin/aws'" >> ~/.bash_profile
-source ~/.bash_profile
 #
-test -n "$AWS_REGION" && echo "PASSED: AWS_REGION is $AWS_REGION" || echo AWS_REGION is not set !!
-test -n "$TF_VAR_region" && echo "PASSED: TF_VAR_region is $TF_VAR_region" || echo TF_VAR_region is not set !!
-test -n "$ACCOUNT_ID" && echo "PASSED: ACCOUNT_ID is $ACCOUNT_ID" || echo ACCOUNT_ID is not set !!
-echo "setup tools run" >> ~/setup-tools.log
-cd ~/environment/tfekscode/lb2
-#curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json -s
-curl -o iam_policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.4.0/docs/install/iam_policy.json
-cd $this
+# test -n "$AWS_REGION" && echo "PASSED: AWS_REGION is $AWS_REGION" || echo AWS_REGION is not set !!
+# test -n "$TF_VAR_region" && echo "PASSED: TF_VAR_region is $TF_VAR_region" || echo TF_VAR_region is not set !!
+# test -n "$ACCOUNT_ID" && echo "PASSED: ACCOUNT_ID is $ACCOUNT_ID" || echo ACCOUNT_ID is not set !!
+# echo "setup tools run" >> ~/setup-tools.log
+# cd ~/environment/tfekscode/lb2
+# #curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json -s
+# curl -o iam_policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.4.0/docs/install/iam_policy.json
+# cd $this
 #
 # final checks - run check.sh
+
 #
